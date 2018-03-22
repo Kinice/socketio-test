6a36b7f3a096f1d99dbff9de51c9bd37d47f0d05
Kinice <szp93@126.com>
2018-03-22 22:52:16 +0800

    Finish demo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -13,6 +14,17 @@ app.get('/', function(req, res){
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('socket message', function(msg) {
+    console.log(msg);
+    io.emit('socket message', msg);
+  })
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
